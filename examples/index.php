e33526b89249efea247da0390778aaa73c5bfca4
elie <sauveterre.elie@gmail.com>
2013-09-15 20:43:48 -0400

    Add examples folder

---
+++ examples/index.php
@@ -0,0 +1,38 @@
+
+<?php
+
+require '../src/Pson.class.php';
+
+class User {
+
+    private $_nom;
+    public $_prenom;
+
+    /**
+     * @FieldClass('Voiture')
+     */
+    private $_voiture;
+
+    // other fiedls or methods ...
+}
+
+class Voiture {
+
+    public $_modele;
+    public $_prix;
+
+}
+$json = '{"_nom":"mike","_prenom":"brandon","_voiture":{"_modele":"Audi A4","_prix":20000}}';
+
+$pson = new Pson();
+
+$user = $pson->fromJson($json, 'User');
+
+echo "<pre>";
+print_r($user);
+echo "<pre>";
+print_r($pson->toJson($user));
+
+
+
+
